a189b7181dcd64923f7012da88189ab2d459d358
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2024-02-19 18:45:29 +0530

    init reactive form ui

--- src/app/reactive-form1/reactive-form1.component.ts
+++ src/app/reactive-form1/reactive-form1.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-reactive-form1',
   templateUrl: './reactive-form1.component.html',
-  styleUrls: ['./reactive-form1.component.scss']
+  styleUrls: ['./reactive-form1.component.scss','./reactive-form1.scss']
 })
 export class ReactiveForm1Component {
 
